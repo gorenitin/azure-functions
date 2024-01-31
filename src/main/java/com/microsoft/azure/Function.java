e38afbd76a3e3a69de0ba099c6bd01874f52220b
gorenitin <gore.nitin@gmail.com>
2024-02-01 05:26:09 +0530

    telimetry example

--- src/main/java/com/microsoft/azure/Function.java
+++ src/main/java/com/microsoft/azure/Function.java
@@ -1,5 +1,8 @@
 package com.microsoft.azure;
 
+import java.util.Optional;
+
+import com.microsoft.applicationinsights.TelemetryClient;
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.HttpMethod;
 import com.microsoft.azure.functions.HttpRequestMessage;
@@ -9,12 +12,13 @@ import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
 
-import java.util.Optional;
-
 /**
  * Azure Functions with HTTP Trigger.
  */
 public class Function {
+	
+	private TelemetryClient telemetry = new TelemetryClient();
+	
     /**
      * This function listens at endpoint "/api/HttpExample". Two ways to invoke it using "curl" command in bash:
      * 1. curl -d "HTTP Body" {your host}/api/HttpExample
@@ -29,14 +33,18 @@ public class Function {
                 HttpRequestMessage<Optional<String>> request,
             final ExecutionContext context) {
         context.getLogger().info("Java HTTP trigger processed a request.");
-
+        telemetry.trackEvent("Java HTTP trigger processed a request.");
+        
         // Parse query parameter
         final String query = request.getQueryParameters().get("name");
         final String name = request.getBody().orElse(query);
-
+        context.getLogger().info("[LOG MESSAGE]");
+        
         if (name == null) {
+            telemetry.trackEvent("Execution failure - Incorrect or missing parameter used.");
             return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
         } else {
+       	   telemetry.trackEvent("Execution success - name parameter = " + name);
             return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name.toUpperCase()).build();
         }
     }
